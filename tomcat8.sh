50ee7b2258c341b075161587a1902fe1e23d33b6
Harish Chander Dalal <harish.chander@hotmail.com>
2017-08-29 18:25:45 +0530

    Update tomcat8.sh

--- tomcat8.sh
+++ tomcat8.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+echo BEGIN
 sudo apt-get update
 sudo apt-get install -y language-pack-en
 sudo apt-get install -y python-software-properties
 sudo apt-add-repository -y ppa:webupd8team/java
 sudo apt-get update
+
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
+
 sudo apt-get install -y tomcat8
 sudo systemctl start tomcat8
 sudo systemctl status tomcat8
-curl ifconfig.me
+
+ip=`curl ifconfig.me`
+echo "your Tomcat8 URL = $ip:8080"
+echo BEGIN
